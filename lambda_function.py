1914ea58cd267b545c61a0c85c2de9e0dae69f47
Dimitry Pletnikov <dimitry@pletnikov.com>
2018-03-16 06:10:08 -0600

    added reads on GSI; related to #1;

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,34 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 import boto3
 import uuid
+from boto3.dynamodb.conditions import Key
+
+def ping_table_key(table_name, key_field, gsi_name=None):
+    random_value = str(uuid.uuid4())
+    
+    table = boto3.resource('dynamodb').Table(table_name)
+    # generating read
+    if gsi_name is None:
+        table.query(
+            KeyConditionExpression=Key(key_field).eq(random_value)
+        )
+    else:
+        table.query(
+            IndexName=gsi_name,
+            KeyConditionExpression=Key(key_field).eq(random_value)
+        )
+    logging.info('Performed read on table `%s` using key `%s`.' % (table_name, key_field))
+    
+    # generating write
+    if gsi_name is None:
+        table.delete_item(
+            Key={
+                key_field: random_value
+            }
+        )
+        logging.info('Performed write on table `%s` using key `%s`.' % (table_name, key_field))
+    else:
+        pass
 
 def lambda_handler(event, context):
     client = boto3.client('dynamodb')
@@ -10,25 +38,16 @@ def lambda_handler(event, context):
     for table_name in client.list_tables()['TableNames']:
         logging.info('Pinging table `%s`..' % table_name)
         
-        key_field = client.describe_table(TableName=table_name)['Table']['KeySchema'][0]['AttributeName']
+        table_description = client.describe_table(TableName=table_name)       
+        key_field = table_description['Table']['KeySchema'][0]['AttributeName']
         logging.info('Found key `%s` in table `%s`.' % (key_field,table_name))
         
-        random_value = str(uuid.uuid4())
+        ping_table_key(table_name, key_field)
         
-        table = boto3.resource('dynamodb').Table(table_name)
-        # generating read
-        table.get_item(
-            Key={
-                key_field: random_value
-            }
-        )
-        logging.info('Performed read on table `%s`.' % table_name)
-        # generating write
-        table.delete_item(
-            Key={
-                key_field: random_value
-            }
-        )
-        logging.info('Performed write on table `%s`.' % table_name)
+        for gsi in table_description['Table'].get('GlobalSecondaryIndexes',[]):
+            gsi_name = gsi['IndexName']
+            gsi_field = gsi['KeySchema'][0]['AttributeName']
+            logging.info('Found GSI `%s` on field `%s` in table `%s`.' % (gsi_name,gsi_field,table_name))
+            ping_table_key(table_name, gsi_field, gsi_name)
         
-    return 'Hello from Lambda'
+    return True
